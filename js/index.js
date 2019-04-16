1bc6968e3f39a65aec15acc6ceb08b807ee0a249
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-16 17:19:49 -0400

    Add focus and doubleclick event listener and handler

--- js/index.js
+++ js/index.js
@@ -5,7 +5,8 @@
 const logo = document.querySelector('.logo-heading');
 const img = document.querySelector('.intro');
 const busImg = document.querySelector('img');
-
+const text = document.querySelector('.content-section');
+const nav = document.querySelector('.main-navigation');
 
 img.addEventListener('mouseover', runEvent);
 
@@ -13,8 +14,20 @@ function runEvent(e) {
     busImg.src = "https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2019/04/696/392/shar-2-KANE-OVERALL-MAGNUS-NEWS.jpg?ve=1&tl=1";
 }
 
-logo.addEventListener('mousemove', runEvent);
+logo.addEventListener('mousemove', runEvent2);
 
-// function runEvent(e) {
-//     logo.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
-// }
+function runEvent2(e) {
+    logo.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+}
+
+text.addEventListener('focus', runEvent3);
+
+function runEvent3(e) {
+    text.textContent = "All your text are belong to me";
+}
+
+nav.addEventListener('dblclick', runEvent4);
+
+function runEvent4(e) {
+    nav.setAttribute('style', 'text-color: pink');
+}
