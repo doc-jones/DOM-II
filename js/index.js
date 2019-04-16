9f444316de1fb36a3d74bb1af6cf1f81fd481eec
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-16 15:45:32 -0400

    Test less: add and remove background: red, Add event listener/handler to report mouse location at logo position

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 // Your code goes here
-// comment to enable commit
 
+
+
+const logo = document.querySelector('.logo-heading');
+const shark = document.querySelector('.intro');
+const busImg = document.querySelector('src');
+
+
+shark.addEventListener('mouseover', runEvent);
+
+// function runEvent(e) {
+//     busImg.setAttribute('src', "https://js-tutorials.nyc3.digitaloceanspaces.com/shark.png");
+// }
+
+logo.addEventListener('mousemove', runEvent);
+
+function runEvent(e) {
+    logo.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+}
