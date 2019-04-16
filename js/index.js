06cac1946b32ba5a3fe7d26ba83367475e5fa796
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-16 16:01:04 -0400

    Add photo replace bus with shark  on moouseover

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@
 
 
 const logo = document.querySelector('.logo-heading');
-const shark = document.querySelector('.intro');
-const busImg = document.querySelector('src');
+const img = document.querySelector('.intro');
+const busImg = document.querySelector('img');
 
 
-shark.addEventListener('mouseover', runEvent);
+img.addEventListener('mouseover', runEvent);
 
-// function runEvent(e) {
-//     busImg.setAttribute('src', "https://js-tutorials.nyc3.digitaloceanspaces.com/shark.png");
-// }
+function runEvent(e) {
+    busImg.src = "https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2019/04/696/392/shar-2-KANE-OVERALL-MAGNUS-NEWS.jpg?ve=1&tl=1";
+}
 
 logo.addEventListener('mousemove', runEvent);
 
-function runEvent(e) {
-    logo.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
-}
+// function runEvent(e) {
+//     logo.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+// }
